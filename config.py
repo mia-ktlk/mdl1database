6b8ba59fd00f2f15544eb9797dce758a3fae2c28
mia-ktlk <mia.kotalik@gmail.com>
2020-03-06 08:03:50 -0700

    heroku changes

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ class Config(object):
         {"name": "MyOpenID", "url": "https://www.myopenid.com"},
     ]
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_JADE_URL') 
+    # or "sqlite:///" + os.path.join(basedir, "app.db")
     # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
 
     BABEL_DEFAULT_LOCALE = "en"
