b0e0f4c8114a9f61cb313883872b2994c07a5593
mia-ktlk <mia.kotalik@gmail.com>
2020-03-06 07:47:23 -0700

    heroku changes

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config(object):
         {"name": "MyOpenID", "url": "https://www.myopenid.com"},
     ]
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    # SQLALCHEMY_DATABASE_URI = 'mysql://myapp@localhost/myapp'
+
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_FOLDER = "translations"
     LANGUAGES = {
@@ -73,5 +73,3 @@ class Config(object):
         ]
     }
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-
-
