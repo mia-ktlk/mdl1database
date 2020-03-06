d4c84aeb4ca3329bce44ef808505f76bf40390dd
mia-ktlk <mia.kotalik@gmail.com>
2020-03-06 00:05:18 -0700

    typo fix

---
+++ .env
@@ -0,0 +1,2 @@
+
+export APP_SETTINGS="config.DevelopmentConfig"
--- config.py
+++ config.py
@@ -1,18 +1,14 @@
-Learn more or give us feedback
 import os
-from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):    
+    DEBUG = False
+    TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = "youwillneverguess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     OPENID_PROVIDERS = [
         {"name": "Google", "url": "https://www.google.com/accounts/o8/id"},
         {"name": "Yahoo", "url": "https://me.yahoo.com"},
@@ -77,3 +73,13 @@ class Config(object):
         ]
     }
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
