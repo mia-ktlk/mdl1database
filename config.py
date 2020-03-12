6e65ab9ee15d3a87b282a1a13fac6fa5c9a0ce4a
mia-ktlk <mia.kotalik@gmail.com>
2020-03-12 12:28:40 -0400

    updates

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ class Config(object):
     ]
    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///" + os.path.join(basedir, "app.db")
-    #SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_FOLDER = "translations"
